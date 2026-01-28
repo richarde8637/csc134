736eb11caa8c9d4db9b808bfaefb80ae65bec6d0
Evan <richarde8637@student.faytechcc.edu>
2026-01-28 22:05:56 +0000

    I added the ability to receive information into code. I set name, price of apples and how many.

--- M2T1/main.cpp
+++ M2T1/main.cpp
@@ -3,14 +3,20 @@
 #include <string>
 
 int main(void) {
-    std::string name = "Jane Smith";
-    int apples = 100;
-    float price = 0.25f;
+    std::string name;
+    int apples;
+    float price;
+    std::cout << "Name: ";
+    std::cin >> name;
+    std::cout << "Number of apples: ";
+    std::cin >> apples;
+    std::cout << "Price per apple: ";
+    std::cin >> price;
     float buyout = price * apples;
     std::cout << std::fixed << std::setprecision(2);
     std::cout << name << std::endl;
     std::cout << "Quantity is " << apples << " apples" << std::endl;
-    std::cout << "Price per apple " << price << " cents" << std::endl;
+    std::cout << "Price per apple " << price << " dollars" << std::endl;
     std::cout << "Price for buyout is " << buyout << " dollars" << std::endl;
     return 0;
     // first time daley was nice
